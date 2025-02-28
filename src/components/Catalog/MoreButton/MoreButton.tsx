d1644682ff2cee91f9dfd6c5ef90c69e7fc0d238
annamalia3000 <a.molch@mail.ru>
2025-02-28 18:08:34 +0300

    fix MoreButton

--- src/components/Catalog/MoreButton/MoreButton.tsx
+++ src/components/Catalog/MoreButton/MoreButton.tsx
@@ -3,24 +3,32 @@ import cn from "classnames";
 import classes from "./moreButton.module.css";
 
 type MoreButtonProps = {
-    loadMoreItems: () => void;
-    loading: boolean;
-    hasMore: boolean;
-  };
+  loadMoreItems: () => void;
+  loading: boolean;
+  hasMore: boolean;
+};
 
-export const MoreButton = ({ loadMoreItems, loading, hasMore }:MoreButtonProps) => {
-    if (!hasMore) {
-        return null;
-      }
+export const MoreButton = ({
+  loadMoreItems,
+  loading,
+  hasMore,
+}: MoreButtonProps) => {
+  if (!hasMore) {
+    return null;
+  }
   return (
     <div className={classes["more-container"]}>
+      {loading ? (
+        <MoreLoader />
+      ) : (
         <button
-        className={cn(classes["more-btn"], 'btn-outline-primary')}
-        onClick={loadMoreItems}
-        disabled={loading}
+          className={cn(classes["more-btn"], "btn-outline-primary")}
+          onClick={loadMoreItems}
+          disabled={loading}
         >
-            {loading? <MoreLoader/>: "Загрузить еще"}
+          {"Загрузить еще"}
         </button>
+      )}
     </div>
-  )
-}
+  );
+};
